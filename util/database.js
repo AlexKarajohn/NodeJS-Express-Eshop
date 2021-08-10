3631aa9c75c02e06f662bcf5380f78d4c632e20c
Alexandros Karagiannis <pp.alexkara@gmail.com>
2021-08-10 11:14:03 +0300

    Added mySQL & refactored to save items in db instead of fs

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 'nodecomplete'
+    password: 'q12wq12wq!@WQ!@WQ',
+    
 });
 
 module.exports = pool.promise();
