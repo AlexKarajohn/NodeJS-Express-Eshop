cbb14e8bed9e77aeb8543ff7942e585955818582
Alexandros Karagiannis <pp.alexkara@gmail.com>
2021-08-17 09:49:19 +0300

    Pagination added on product lists

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,13 +3,32 @@ const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit')
+
+const ITEMS_PER_PAGE = 3 ;
+
+
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems ;
   Product.find()
+  .countDocuments()
+  .then((numProducts)=>{
+    totalItems = numProducts;
+    return   Product.find()
+    .skip((page-1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
   .then(products=>{
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Index',
       path: '/',
+      currentPage : page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage : page > 1,
+      nextPage: page + 1,
+      previousPage : page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   })
   .catch(err=>{
@@ -21,13 +40,27 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems ;
   Product.find()
+  .countDocuments()
+  .then((numProducts)=>{
+    totalItems = numProducts;
+    return   Product.find()
+    .skip((page-1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
   .then(products=>{
     res.render('shop/product-list', {
       prods: products,
-      pageTitle: 'Product List',
+      pageTitle: 'Products',
       path: '/products',
-      
+      currentPage : page,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage : page > 1,
+      nextPage: page + 1,
+      previousPage : page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   })
   .catch(err=>{
@@ -225,5 +258,4 @@ exports.getInvoice = (req,res,next) =>{
     // file.pipe(res);
   })
   .catch(err=>next(err))
-  
 }
